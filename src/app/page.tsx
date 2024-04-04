b963681b4ef9ae1df35d4473b49af2a3d6eb16ba
Tristan Rechenberger <rechenberger@me.com>
2024-04-04 13:19:17 +0200

    🎉 next-super-action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ActionButton } from '@/super-action/button/ActionButton'
 
 export default function Page() {
   return (
     <>
-      <ActionButton
-        tryToast
-        action={async () => {
-          'use server'
-          return {
-            toastTitle: 'Success',
-            toastDescription: 'You did it!',
-          }
-        }}
-        command={{}}
-      >
-        My First Action
-      </ActionButton>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+          <CardTitle>
+            <CardHeader>My First Action</CardHeader>
+          </CardTitle>
+          <CardContent>
+            <ActionButton
+              tryToast
+              action={async () => {
+                'use server'
+                return {
+                  toastTitle: 'Success',
+                  toastDescription: 'You did it!',
+                }
+              }}
+              command={{}}
+            >
+              My First Action
+            </ActionButton>
+          </CardContent>
+        </Card>
+      </div>
     </>
   )
 }
